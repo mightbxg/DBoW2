4df2a7f4a07f9b53bb94a7b7d6a6c64e12ea8ed3
tiliang <tiliang@orbbec.com>
2020-12-01 16:25:06 +0800

    add time of loading vocabulary

--- tests/OriginCompare.cpp
+++ tests/OriginCompare.cpp
@@ -5,9 +5,11 @@
 #include <chrono>
 #include <iostream>
 
-using ORBVocabulary = DBoW2::TemplatedVocabulary<DBoW2::FORB, 10>;
 using namespace std;
 using namespace chrono;
+
+using ORBVocabulary = DBoW2::TemplatedVocabulary<DBoW2::FORB, 10>;
+using ORBVocabularyOrigin = origin::TemplatedVocabulary<origin::FORB::TDescriptor, origin::FORB>;
 
 int main(int argc, char* argv[])
 {
@@ -16,19 +18,29 @@ int main(int argc, char* argv[])
         return 0;
     }
 
+    cout << "time of loading volcabulary -----------"<<endl;
+    ORBVocabularyOrigin voc_origin;
     {
-        ORBVocabulary voc;
         auto time_start = high_resolution_clock::now();
-        voc.loadFromBinaryFile(argv[1]);
+        voc_origin.loadFromBinaryFile(argv[1]);
+        auto time_end = high_resolution_clock::now();
+        double time_cost = duration_cast<nanoseconds>(time_end - time_start).count() * 1e-6;
+        cout << "origin: " << time_cost << " ms" << endl;
+    }
+
+    ORBVocabulary voc_bin;
+    {
+        auto time_start = high_resolution_clock::now();
+        voc_bin.loadFromBinaryFile(argv[1]);
         auto time_end = high_resolution_clock::now();
         double time_cost = duration_cast<nanoseconds>(time_end - time_start).count() * 1e-6;
         cout << "bin  : " << time_cost << " ms" << endl;
     }
 
+    ORBVocabulary voc_fsbin;
     {
-        ORBVocabulary voc;
         auto time_start = high_resolution_clock::now();
-        voc.loadFromFsBinFile(argv[2]);
+        voc_fsbin.loadFromFsBinFile(argv[2]);
         auto time_end = high_resolution_clock::now();
         double time_cost = duration_cast<nanoseconds>(time_end - time_start).count() * 1e-6;
         cout << "fsbin: " << time_cost << " ms" << endl;
